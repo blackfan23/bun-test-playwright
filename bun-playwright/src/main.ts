f49870e131073eea297fc94573493d97bee57ec4
Julius <transferx23@googlemail.com>
2024-07-28 17:42:40 +0200

    added latest deps and bun

--- bun-playwright/src/main.ts
+++ bun-playwright/src/main.ts
@@ -2,13 +2,17 @@ import { chromium } from 'playwright';
 
 console.log('Running bun-playwright with version: ', Bun.version);
 
-const browser = await chromium.connectOverCDP('http://localhost:9555');
-const defaultContext = browser.contexts()[0];
-const page = await defaultContext.newPage();
-page.goto('https://google.com');
+try {
+  const browser = await chromium.connectOverCDP('http://localhost:9555');
+  const defaultContext = browser.contexts()[0];
+  const page = await defaultContext.newPage();
+  await page.goto('https://google.com');
 
-// get page title
-const title = await page.title();
-console.log(`Title of the page is ${title}`);
-await page.waitForTimeout(5000);
-await browser.close();
+  // get page title
+  const title = await page.title();
+  console.log(`Title of the page is ${title}`);
+  await page.waitForTimeout(5000);
+  await browser.close();
+} catch (error) {
+  console.error(error);
+}
